32019430db25050a480911fdc794efb2ec5fa3e4
bdey05 <73259986+bdey05@users.noreply.github.com>
2021-02-16 16:30:35 -0500

    Add files via upload

---
+++ javascriptstuff/javascript/arrays.js
@@ -0,0 +1,10 @@
+var array = ["fat", "cat"]
+console.log(array[0])
+
+var newArr = [["fat", "man"], ["fat", 3]]
+console.log(newArr[1][1])
+console.log(43)
+
+array[0] = 4
+
+console.log(array[0])
---
+++ javascriptstuff/javascript/test.js
@@ -0,0 +1,28 @@
+// in line - comment
+/* This is a multiline comment*/
+
+/*DATA TYPES*/
+var myName = 'Beau' //can be used in the whole program
+myName = 8
+
+let ourName = 'Stuff' //only within scope 
+
+const pi = 3.14 //variable that can never be changed
+
+var a; //declaring
+var b = 12; //declaring + initializing
+
+a = 7
+
+//Print to console
+console.log(a)
+
+console.log(b+3)
+console.log(a + " javascript")
+console.log(a+=2)
+
+console.log("I am a \"double quote\"")
+
+var x = "red"
+console.log(x.length)
+console.log(x[1])
